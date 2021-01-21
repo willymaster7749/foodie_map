63c6de5e08f3f8192e0754ae6d0b75acc3d92244
Willy Huang <starwars78910@gmail.com>
2021-01-22 00:11:41 +0800

    deploy

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FindRestaurantsAPI } from "../commonAPI";
 
-const API_URL = "https://ntu-foodiez.herokuapp.com//api/auth/";
+const API_URL = "https://ntu-foodiez.herokuapp.com/api/auth/";
 
 const register = (username, email, password) => {
     return axios.post(API_URL + "register", {
